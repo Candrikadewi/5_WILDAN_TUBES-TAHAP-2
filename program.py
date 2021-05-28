1c9a23586cce745b7c2c4013cec5cd2e3e3dee23
Candrika Dewi <candrikaa8@gmail.com>
2021-05-28 18:18:45 +0700

    Pengerjaan Tubes

--- program.py
+++ program.py
@@ -8,9 +8,27 @@ rose = 0
 for win in range (0,2) :
     print (listdata[rose])
     rose+=1
-konfirmasi: str(input("Apakah semua data yang diperlukan sudah Anda siapkan? (Sudah/Belum):"))
+konfir = str(input("Apakah semua data yang diperlukan sudah Anda siapkan? (Sudah/Belum):"))
+konfir2 = konfir.title()
 
-if konfirmasi=="Sudah":
+if konfir2=="Sudah" :
     print("Silakan mengisi identitas Anda dengan seksama dan jawablah pertanyaan sesuai dengan kondisi Anda saat ini")
-else:
-    print("Mohon persiapkan dahulu seluruh data yang diperlukan!")
+    nama = str(input("Nama Lengkap="))
+    nik = int(input("NIK="))
+    nama_ayah= str(input("Nama Ayah="))
+    nama_ibu= str(input("Nama Ibu="))
+    print("Kelahiran:")
+    tanggal = int(input("Tanggal Lahir="))
+    bulan = int(input("Bulan Lahir="))
+    tahun = int(input("Tahun Lahir="))
+    lahir = tanggal + (bulan*30) + (tahun*365)
+
+    print("Tanggal registrasi:")
+    tanggalregist = int(input("Tanggal Registrasi="))
+    bulanregist = int(input("Bulan Registrasi="))
+    tahunregist = int(input("Tahun Registrasi="))
+    registrasi = tanggalregist + (bulanregist * 30) + (tahunregist * 365)
+    tahunusia = int((registrasi - lahir) / 365)
+    bulanusia = int(((registrasi - lahir) % 365) / 30)
+    hariusia  = int(((registrasi - lahir) % 365) % 30)
+    print("Usia=",tahunusia,"tahun")
